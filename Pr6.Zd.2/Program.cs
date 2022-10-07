9e88de881e24b0933126b6324752120771e56484
USER <USER@DESKTOP-TRQG01D>
2022-10-07 13:57:15 +0300

    Добавьте файлы проекта.

---
+++ Pr6.Zd.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            Console.WriteLine("Введите значение переменной x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            double y;
+            Console.WriteLine("Введите значение переменной y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0} {1}", -x, y);
+            double S;
+            S = -x * y;
+            Console.WriteLine("S= -x*y = {0}", S);
+
+        }
+    }
+}
